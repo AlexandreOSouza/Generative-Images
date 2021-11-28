ad06cdeccbc2dfde115a13aa6cb4b499092d3fcf
Alexandre <AlexandreOSouza>
2021-11-27 21:06:11 -0300

    feat: I dont know

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 const { createCanvas } = require('canvas')
+var crypto = require('crypto');
+
 
 const { LAYERS } = require('./config/layersConfig')
 const { getNumberOfFilesInFolder, normalizePath, loadImageFromFile } = require('./utils/assetsUtils.js')
 const { randomInteger } = require('./utils/randomUtils')
+
+var name = 'braitsch';
+var hash = crypto.createHash('md5').update(name).digest('hex');
+console.log(hash); // 9b74c9897bac770ffc029102a200c5de
 
 const width = 508
 const height = 508
@@ -14,6 +20,10 @@ context.fillStyle = '#fff'
 context.fillRect(0, 0, width, height)
 
 function createFile () {
+
+  let layerLeght = LAYERS.length
+  let index = 0
+  hash = crypto.createHash('md5').update(name).digest('hex');
   LAYERS?.forEach(layer => {
     let numberOfFiles = getNumberOfFilesInFolder(layer.name)
     let assetsIndex = randomInteger(numberOfFiles)
@@ -25,8 +35,15 @@ function createFile () {
       }
       context.drawImage(img, layer.x, layer.y, layer.w, layer.h)
       fs.writeFileSync(`./images_output/new_image_test.png`, canvas.toBuffer("image/png"));
+      index++
     })
   })
+
+  if (index === layerLeght) {
+    return hash
+  }
 }
 
-createFile()
+console.log(createFile())
+
+console.log('do')
